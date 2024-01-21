adf0ed381a1edbdfc05d4ce6a680d39ca22972ce
Vandhana Baskar <vandhubaskar34@gmail.com>
2024-01-21 22:59:43 +0530

    <update>

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { Directive, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+
+
+
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    Directive,
+  ],
+
+})
+export class AppModule { }
